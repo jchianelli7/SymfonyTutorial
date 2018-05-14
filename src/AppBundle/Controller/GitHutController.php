6bac16e02b7a1ba7485e921fac35a162e22c1553
Josh Chianelli <joshchianelli@gmail.com>
2018-05-13 23:11:15 -0500

    finished stubbing more profile info

--- src/AppBundle/Controller/GitHutController.php
+++ src/AppBundle/Controller/GitHutController.php
@@ -14,11 +14,11 @@ class GitHutController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('githut/index.html.twig',[
+        return $this->render('githut/index.html.twig', [
             'avatar_url' => 'https://avatars0.githubusercontent.com/u/7884719?v=4',
-            'name'       => 'Josh Chianelli',
-            'login'      => 'jchianelli7',
-            'details'    => [
+            'name' => 'Josh Chianelli',
+            'login' => 'jchianelli7',
+            'details' => [
                 'company' => 'Josh',
                 'location' => 'Milwaukee, WI',
                 'joined_on' => 'Joined on 13th May 2018',
@@ -28,6 +28,28 @@ class GitHutController extends Controller
                 'Public Repos' => 7,
                 'Followers' => 1123,
                 'Following' => 1132,
+            ],
+            'repo_count' => 100,
+            'most_stars' => 99,
+            'repos' => [
+                [
+                    'name' => 'example 1',
+                    'url' => 'https://github.com/jchianelli7',
+                    'stargazers_count' => 46,
+                    'description' => 'desc. 1',
+                ],
+                [
+                    'name' => 'example 2',
+                    'url' => 'https://github.com/jchianelli7',
+                    'stargazers_count' => 46,
+                    'description' => 'desc. 2',
+                ],
+                [
+                    'name' => 'example 3',
+                    'url' => 'https://github.com/jchianelli7',
+                    'stargazers_count' => 46,
+                    'description' => 'desc. 3',
+                ],
             ],
         ]);
     }
